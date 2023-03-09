e33e9cbbec07ba1e98fd90309011b8a315a76bf1
Tom Groot <4732713+Tomgroot@users.noreply.github.com>
2023-03-09 23:06:27 +0100

    Remove overview results

---
+++ resources/views/emails/resultMail.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Resultaat van jouw enneagram test</title>
+    <style>
+        h1 {
+            font-size: 2rem;
+        }
+    </style>
+</head>
+<body>
+<h1>Je resultaat is bekend!</h1>
+<p>Bedankt voor het voltooien van de enneagram test. Je resultaat is bekend.</p>
+<a href="{{$mailData['uri']}}">Klik hier voor je resultaat</a>
+</body>
+</html>
